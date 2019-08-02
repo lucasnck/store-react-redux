ac4b158161778f8b532b60e8c6eb47e1f9d8201a
Lucas Ribeiro <luucasnck@gmail.com>
2019-08-01 23:41:15 -0300

    Route path adjust

--- precache-manifest.3f9719a743a28c16949726f943d9ae46.js
+++ precache-manifest.3f9719a743a28c16949726f943d9ae46.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9f35bdedf195345bc7a3c9c871c1b15a",
+    "revision": "c6df47c48f375c009eb53e78d13a2ed9",
     "url": "/store-react-redux/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/store-react-redux/static/css/2.22a7d4ef.chunk.css"
   },
   {
-    "revision": "47f135016ce719bffcd6",
+    "revision": "bb29ce02c029d7d29edb",
     "url": "/store-react-redux/static/css/main.9a09ed06.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/store-react-redux/static/js/2.e3d4dcd5.chunk.js"
   },
   {
-    "revision": "47f135016ce719bffcd6",
-    "url": "/store-react-redux/static/js/main.343c4f78.chunk.js"
+    "revision": "bb29ce02c029d7d29edb",
+    "url": "/store-react-redux/static/js/main.de9fffee.chunk.js"
   },
   {
     "revision": "4d08067aeb289c566b9f",
