9e5fbf6a13405c13e2dec79b65eebcf5fc4a2b88
Lucas Ribeiro <luucasnck@gmail.com>
2019-08-01 23:34:48 -0300

    Initial dist subtree commit

---
+++ precache-manifest.28d7ea5ed49831ab3e35bac5f68d3066.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f35bdedf195345bc7a3c9c871c1b15a",
+    "url": "/store-react-redux/index.html"
+  },
+  {
+    "revision": "9bdf7d1b52c038d11867",
+    "url": "/store-react-redux/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "47f135016ce719bffcd6",
+    "url": "/store-react-redux/static/css/main.9a09ed06.chunk.css"
+  },
+  {
+    "revision": "9bdf7d1b52c038d11867",
+    "url": "/store-react-redux/static/js/2.e3d4dcd5.chunk.js"
+  },
+  {
+    "revision": "47f135016ce719bffcd6",
+    "url": "/store-react-redux/static/js/main.343c4f78.chunk.js"
+  },
+  {
+    "revision": "4d08067aeb289c566b9f",
+    "url": "/store-react-redux/static/js/runtime~main.18640813.js"
+  }
+]);
